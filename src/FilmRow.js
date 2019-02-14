af29521840152feb407a41edd928319a09aca6d9
Leonardo Rodriguez <leo@ool-ad030072.dyn.optonline.net>
2019-02-14 15:47:54 -0500

    initial commit

---
+++ src/FilmRow.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import FilmPoster from './FilmPoster.js';
+
+class FilmDetails extends Component {
+  render() {
+    return (
+        <div className="film-row">
+         <FilmPoster film={this.props.film} />
+      
+        <div className="film-summary">
+          <h1>{this.props.film.title}</h1>
+          <p>{new Date(this.props.film.release_date).getFullYear()}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FilmDetails
